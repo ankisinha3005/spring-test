98e5e77ecde6ca53d8ce15ec52c0afef3236cc06
ankisinha3005 <ankit.sinha3005@gmail.com>
2021-09-04 14:57:52 +0530

    first commit

---
+++ src/test/java/com/ankit/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ankit.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//Don't need to add extend with after a certain version
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
